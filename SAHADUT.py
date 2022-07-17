bee7d204b88a231ab1151b5ded7643a806225dbc
sahadut2022 <109257820+sahadut2022@users.noreply.github.com>
2022-07-17 08:59:49 +0600

    Create SAHADUT.py
    pkg update
    
    pkg upgrade
    
    pkg install git
    
    pkg install python
    
    pkg install python2
    
    pip install requests
    
    pip install mechanize
    
    pip install lolcat
    
    pip install bs4
    
    rm -rf sahadut
    
    git clone https://github.com/sahadut2022/sahadut.git
    
    cd sahadut
    
    python SAHADUT.py

---
+++ SAHADUT.py
@@ -0,0 +1,27 @@
+import os,time
+def clear():
+	os.system('clear')
+def banner():
+	clear()
+	print("""
+\033[1;37m    ###    ##    ## #### ##    ##  ######
+   ## ##   ##   ##   ##  ###   ## ##    ##    
+  ##   ##  ##  ##    ##  ####  ## ##  
+ ##     ## #####     ##  ## ## ## ##   ####
+ ######### ##  ##    ##  ##  #### ##    ## 
+ ##     ## ##   ##   ##  ##   ### ##    ##  
+ ##     ## ##    ## #### ##    ##  ######
+(!)========================================
+(!) Author   : MD SAHADUT HOSAN
+(!) Guthub   : sahadut10
+(!) Facebook : Md Sahadut Hosan
+(!) Type     : Paid
+\033[1;37m(!)========================================""")
+def Update():
+    banner()
+    print("\n\x1b[1;92m Updating Soon :-/")
+    print(' Please Wait For Update 😊 ')
+    print(' Join Over Facebook Group')
+    os.system('xdg-open https://www.facebook.com/profile.php?id=100081222665808');time.sleep(3)
+    input('')
+Update()
